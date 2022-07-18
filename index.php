44023f10c252a8af1b626573186758d2def4e52b
JinseoMyeon <dlwlstj0303@naver.com>
2022-07-18 10:20:38 +0900

    feat: add index.php file

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+    session_start();
+    require_once("./config.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="url shortener">
+    <title>씨발 | 링크 단축 서비스</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+    <img src="./assets/images/ssibal.svg" class="ignore logo">
+    <h1 class="title ignore">한 번 들으면 잊을 수 없는<br>링크 단축 서비스.</h1>
+    <p class="urltext ignore">단축할 링크 원본</p>
+    <?php
+    if (isset($_SESSION['success'])) {
+        echo "<p class='success'>" . $_SESSION['success'] . "</p>";
+        unset($_SESSION['success']);
+    }
+    if (isset($_SESSION['error'])) {
+        echo "<p class='alert'>" . $_SESSION['error'] . "</p>";
+        unset($_SESSION['error']);
+    }
+    if (isset($_GET['error']) && $_GET['error'] == 'db') {
+        echo "<p class='alert'>데이터베이스에 연결하는 데 실패했습니다.</p>";
+    }
+    if (isset($_GET['error']) && $_GET['error'] == 'inurl') {
+        echo "<p class='alert'>잘못된 URL로 접근하셨습니다.</p>";
+    }
+    if (isset($_GET['error']) && $_GET['error'] == 'dnp') {
+        echo "<p class='alert'>오류가 발생했습니다.</p>";
+    }
+    ?>
+    <form method="POST" action="functions/shorten.php">
+        <div>
+            <div>
+                <input type="url" id="input" value="https://" name="url" class="input" placeholder="단축하려는 링크를 입력해주세요.">
+            </div>
+            <div>
+                <p class="ssibal ignore">ssib.al/</p>
+                <img class="arrow ignore" src="./assets/images/arrow.svg">
+                <input type="text" id="custom" name="custom" class="inputsmall" placeholder="원하는 링크 이름(선택)">
+            </div>
+        </div>
+        <input type="submit" class="submit" value="링크 줄이기">
+        <img src="./assets/images/warrow.svg" class="warrow ignore">
+    </form>
+    <div class="infodiv">
+        <img src="./assets/images/warning.svg" class="ignore">
+        <p class="bold ignore" style="font-size:16px; margin-left:35px; margin-top:-25px; color:#333333;">이런 사이트는 링크를 단축할 수 없어요!</p>
+    </div>
+    <a href="https://github.com/jinseomyeon/ssib.al">
+        <img src="./assets/images/github.svg" class="github ignore">
+    </a>
+    <div class="rightdiv"></div>
+</body>
+</html>
