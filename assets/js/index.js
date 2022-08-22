c95b26d5e8c89e299488a820b3e23cbc4d74d7f8
JinseoMyeon <dlwlstj0303@naver.com>
2022-08-22 17:39:19 +0900

    feat: add shorten-site warning msg

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-
-var button = document.getElementById("shortenButton");
-if(window.innerWidth < 768) {
-    button.innerText("");
-}
-else if(window.innerWidth >= 768) {
-    button.innerText("링크 줄이기");
-}
