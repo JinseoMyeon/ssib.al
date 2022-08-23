33b55e91db66299f965cfdaaceb0d25b0f993476
JinseoMyeon <dlwlstj0303@naver.com>
2022-08-23 20:06:10 +0900

    chore: change some error messages

--- functions/shorten.php
+++ functions/shorten.php
@@ -19,7 +19,7 @@ if (!$_POST['custom'] == '') {
     
     else {
         $errors            = true;
-        $_SESSION['error'] = '<p class="ignore shortenlink" style="color:#fe5151;">' . "ssib.al/" . $_POST['custom'] . "</p><p class='ignore shorten'>은 이미 존재합니다.</p>";
+        $_SESSION['error'] = '<p class="ignore shortenlink" style="color:#fe5151;">' . "ssib.al/" . $_POST['custom'] . "</p><p class='ignore shorten'>링크는 이미 존재합니다.</p>";
     }
 }
 
