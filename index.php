8a5be47e7e7bb244c3d01acf24fee1a706348e90
JinseoMyeon <dlwlstj0303@naver.com>
2022-08-24 20:29:01 +0900

    feat: support darkmode development

--- index.php
+++ index.php
@@ -56,16 +56,15 @@
     </a>
     <div class="infodiv">
         <img src="./assets/images/warning.svg" class="ignore icon">
-        <p class="bold msg ignore" style="position: relative; margin-left:1.75rem; margin-top: -1.5rem; color:#333333;">이런 사이트는 링크를 단축할 수 없어요.</p>
+        <p class="bold msg ignore warning">이런 사이트는 링크를 단축할 수 없어요.</p>
         <p class="ignore paragraph m-2">대한민국 법률 상 <strong>불법, 유해 사이트</strong>로 지정된 사이트</p>
         <p class="ignore paragraph m-1">해킹, 스미싱 등 <strong>불법적인 용도</strong>로 사용하는 사이트</p>
-        <p class="ignore paragraph m-1"><strong>상업적인 용도</strong>로 사용하는 사이트</p>
-        <p class="ignore paragraph p-small m-05" style="color: #CDCDCD; font-weight: 400;">(설마 이 링크로...?)</p>
+        <p class="ignore paragraph m-1 "><strong>상업적인 용도</strong>로 사용하는 사이트</p>
+        <p class="ignore paragraph p-small m-05 dline" style="color: #CDCDCD; font-weight: 400;">(설마 이 링크로...?)</p>
         <p class="ignore paragraph m-1">이외 운영자가 <strong>링크 단축 서비스를 제공할 수 없다</strong>고 판단하는 사이트</p>
-        <p class="ignore paragraph p-small m-05" style="color: #CDCDCD; font-weight: 400;">이런 사이트는 운영자에 의해 무통보 삭제될 수 있어요.</p>
+        <p class="ignore paragraph p-small m-05 dline">이런 사이트는 운영자에 의해 무통보 삭제될 수 있어요.</p>
         <p class="ignore paragraph p-small m-2">이 사이트는 <strong>링크 단축 서비스만</strong> 제공하고 있습니다.<br>
             이 서비스의 운영자는 해당 링크 단축 서비스를 이용해 발생하는<br>어떠한 사건에도 <strong>책임을 지지 않습니다.</strong></p>
     </div>
-    <script src="./assets/js/index.js"></script>
 </body>
 </html>
