39e802a8fe68c0bef3b8510e987ed032a2dcdd3e
JinseoMyeon <dlwlstj0303@naver.com>
2022-09-04 20:51:39 +0900

    feat: count how many times link used

--- functions/redirect.php
+++ functions/redirect.php
@@ -1,12 +1,24 @@
 <?php
 
 require_once("UrlShortener.php");
+require_once("../config.php");
 
 $urlShortener = new UrlShortener();
 
 if (isset($_GET['secret'])) {
+
+    $this->db = new mysqli(HOST_NAME, USER_NAME, USER_PASSWORD, DB_NAME);
+    if ($this->db->connect_errno) {
+        header("Location: ../index.php?error=db");
+        die();
+    }
+
     $uniqueCode = $_GET['secret'];
     $orignalUrl = $urlShortener->getOrignalURL($uniqueCode);
+    $getCount = $this->db->query("SELECT used_count FROM link WHERE code = '{$uniqueCode}'");
+    $getCount = $getCount + 1;
+    $updateInDatabase = $this->db->query("UPDATE link SET used_count = '{$getCount}' WHERE code = '{$uniqueCode}'");
+    $updateInDatabase = $this->db->query("UPDATE link SET last_used = NOW() WHERE code = '{$uniqueCode}'");
     header("Location: {$orignalUrl}");
     die();
 }
