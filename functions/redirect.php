5a2afbdf52db9ad1f47af36a6f64adba7aca8f4a
JinseoMyeon <dlwlstj0303@naver.com>
2022-09-04 21:32:25 +0900

    feat: add count how many times link used

--- functions/redirect.php
+++ functions/redirect.php
@@ -10,8 +10,7 @@ if (isset($_GET['secret'])) {
     $uniqueCode = $_GET['secret'];
     $orignalUrl = $urlShortener->getOrignalURL($uniqueCode);
     $rows = $database->query("SELECT used_count FROM link WHERE code = '{$uniqueCode}'");
-    $getCount = $rows->fetch_object();
-    $count = int($rows->fetch_object()) + 1;
+    $count = ($rows->fetch_object()->used_count) + 1;
     $updateInDatabase = $database->query("UPDATE link SET used_count = '{$count}' WHERE code = '{$uniqueCode}'");
     header("Location: {$orignalUrl}");
     die();
