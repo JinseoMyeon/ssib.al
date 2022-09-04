fc904278f855e312416634a367869271fd60cf48
JinseoMyeon <dlwlstj0303@naver.com>
2022-09-04 22:18:37 +0900

    fix: try to fix some bugs

--- functions/redirect.php
+++ functions/redirect.php
@@ -8,10 +8,11 @@ $urlShortener = new UrlShortener();
 if (isset($_GET['secret'])) {
     $database = new mysqli(HOST_NAME, USER_NAME, USER_PASSWORD, DB_NAME);
     $uniqueCode = $_GET['secret'];
+    $getUniqueCode = $uniqueCode;
     $orignalUrl = $urlShortener->getOrignalURL($uniqueCode);
-    $rows = $database->query("SELECT used_count FROM link WHERE code = '{$uniqueCode}'");
+    $rows = $database->query("SELECT * FROM link WHERE code = '{$getUniqueCode}' AND url = '{$orignalUrl}' LIMIT 1");
     $count = ($rows->fetch_object()->used_count) + 1;
-    $updateInDatabase = $database->query("UPDATE link SET used_count = '{$count}' WHERE code = '{$uniqueCode}'");
+    $updateInDatabase = $database->query("UPDATE link SET used_count = '{$count}', last_used = NOW() WHERE code = '{$getUniqueCode}' AND url = '{$orignalUrl}'");
     header("Location: {$orignalUrl}");
     die();
 }
