e2d47175ab8230ce3f66f1c0cd6f61b3b44e1a4c
JinseoMyeon <dlwlstj0303@naver.com>
2023-02-07 13:27:40 +0900

    chore: change version

--- index.php
+++ index.php
@@ -205,7 +205,7 @@
                 <div id="bFooter">
                     <p class="footerText">Copyright 2022-2023, <strong>JinseoMyeon</strong> All Rights Reserved.</p>
                     <p class="footerText">Email : i@ssib.al | GitHub : <a href="https://ssib.al/source">ssib.al/source</a></p>
-                    <p class="footerBoldText">ssib.al v2.1.1</p>
+                    <p class="footerBoldText">ssib.al v2.1.2</p>
                 </div>
             </div>
         </div>
